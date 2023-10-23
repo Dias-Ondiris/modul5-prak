cf38ed236eaa3f50c721d1973ae366d6a1cf615b
Dias-Ondiris <dias.ondiris2@gmail.com>
2023-10-23 14:42:55 +0600

    Добавьте файлы проекта.

---
+++ modul5 prak/DemoClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_prak
+{
+    public class DemoClass
+    {
+        public void CallerMethod()
+        {
+            try
+            {
+                CalledMethod();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение перехвачено в CallerMethod: {ex.Message}");
+            }
+        }
+        public void CalledMethod()
+        {
+            throw new InvalidOperationException("Ошибка в CalledMethod");
+        }
+    }
+
+}
---
+++ modul5 prak/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_prak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new DemoClass();
+            demo.CallerMethod();
+            Console.ReadKey();
+        }
+    }
+}
